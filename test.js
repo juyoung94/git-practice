de50ef7c091d26297fbdd2c670e1b4977d7bd2e8
juyoung <deepblue920@naver.com>
2023-06-09 17:01:48 +0900

    add user class

--- test.js
+++ test.js
@@ -1,2 +1,17 @@
 console.log("hello!");
 let user_array = ["juyoung", "donghwa", "suhyung", "dongheok", "junkyu"];
+
+class User {
+    /**
+     * @param name : 이름
+     * @param type : 포지션
+     * @param sex : 성별
+     * */
+    constructor (name, type, sex) {
+        this.name = name;
+        this.type = type;
+        this.sex = sex;
+    }
+}
+
+let juyoung = new User("juyoung", "FE", "F");
